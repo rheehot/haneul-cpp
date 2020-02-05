91c79d9b7780c3a2e2b4d08afeafc13f922c40fe
suhdonghwi <hwidongsuh@gmail.com>
2020-02-06 03:09:22 +0900

    초기 커밋

---
+++ src/Test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "Test.h"
+
+void print_hello(int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    std::cout << "Hello!\n";
+  }
+}
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "Test.h"
+int main()
+{
+  print_hello(10);
+  return 0;
+}
