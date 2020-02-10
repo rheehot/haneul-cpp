cdd5c2f1f5f64d47703e1a80414063278aa08573
suhdonghwi <hwidongsuh@gmail.com>
2020-02-10 18:18:08 +0900

    커맨드라인 파일 입력 추가

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,23 @@
 #include "Constants/ConstInteger.hpp"
 #include "Parser.hpp"
 
-int main() {
-  haneul::Parser::InputType input = {
-      5,   0,   0,   0,   0,   0,   0,   0,   2,   0,   0,   0,  0, 0, 0,
-      0,   2,   236, 136, 152, 49,  0,   0,   0,   0,   0,   0,  0, 2, 236,
-      136, 152, 50,  0,   0,   0,   0,   0,   0,   0,   3,   1,  0, 0, 0,
-      0,   0,   0,   0,   10,  3,   234, 176, 129, 2,   0,   21, 0, 0, 0,
-      0,   0,   0,   255, 255, 255, 255, 255, 255, 255, 207, 0,  0, 0, 0,
-      0,   0,   0,   3,   0,   0,   0,   1,   3,   0,   0,   0,  0, 0, 0,
-      0,   2,   236, 136, 152, 49,  0,   0,   0,   1,   3,   0,  0, 0, 0,
-      0,   0,   0,   2,   236, 136, 152, 50,  0,   0,   0,   1,  8};
-  haneul::Parser parser(input);
-  auto result = parser.parse_constant();
+int main(int argc, char **argv) {
+  if (argc <= 1) {
+    std::cerr << "인수를 입력해주세요!" << std::endl;
+    return EXIT_FAILURE;
+  }
 
-  result->dump();
+  haneul::Parser parser(argv[1]);
+  auto [const_table, code] = parser.parse_program();
+
+  for (const auto &c : const_table) {
+    c->dump();
+  }
+
+  for (const auto &inst : code) {
+    inst.dump();
+  }
+
   // std::cout << result.show() << std::endl;
   // std::cout << parser.par() << '\n';
   return 0;
